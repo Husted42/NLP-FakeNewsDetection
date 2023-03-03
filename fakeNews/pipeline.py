286c61a403471846ce660ddf9a84258044d7e8ae
D1NM04R <118982538+skywong21@users.noreply.github.com>
2023-03-03 12:18:22 +0100

    Update pipeline.py

--- fakeNews/pipeline.py
+++ fakeNews/pipeline.py
@@ -1,14 +1,22 @@
 import re
 import csv
+import os
 
 testSample = 'news_sample.csv'
 
+def findDic():
+    print(os.getcwd())
+    (os.chdir(r'dataScience\fakeNews'))
+    print(os.getcwd())
+    print(os.listdir())
+
 def getCsv(inp):
-    with open('news_sample.csv', 'r') as input_file, open('redactedSample.csv','w', newline = '') as output_file:
+    with open(inp, 'r') as input_file, open('redactedSample.csv','w', newline = '') as output_file:
         csv_reader = csv.reader(input_file)
         csv_writer = csv.writer(output_file)
         for row in csv_reader:
             new_row = [cell.lower() for cell in row]
             csv_writer.writerow(new_row)
 
+findDic()
 getCsv(testSample)
